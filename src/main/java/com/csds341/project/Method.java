8421e10f4e464d258efe80a52b926ba07e8779cf
Tola <tdo18@case.edu>
2024-04-17 23:21:01 -0400

    high level metthods added

---
+++ src/main/java/com/csds341/project/Method.java
@@ -0,0 +1,45 @@
+/**
+ * Project: CSDS 341 Project Group 6
+ * Database: The Forum
+ * Version: 1.0
+ * Authors:
+ *  - Mikey Maldonado (mxm1667)
+ *  - Mike Zhang (zxz1233)
+ *  - Tola Oshomoji (tdo18)
+ */
+package com.csds341.project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Method {
+
+    // High level methods for now until the interface is made
+    public void addnewUser(String username){
+     
+    }
+
+    public void createGroup(String name, String owner_name){
+     
+    }
+
+    public void joinGroup(String username, String group_name){
+     
+    }
+
+    public void postThread(String username, String title){
+     
+    }
+
+    public void seeAvailableThreads(int thread_id, String group_name){
+     
+    }
+    public void seegroupMemebers(String group_name){
+     
+    }
+
+}
