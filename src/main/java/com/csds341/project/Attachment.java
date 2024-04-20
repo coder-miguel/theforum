da74a02237b8f3b952a8ff4d2a9d3889af70f15c
kelifire <mom005@mymail.tc3.edu>
2024-04-20 13:38:27 -0400

    Database 2

---
+++ src/main/java/com/csds341/project/Attachment.java
@@ -0,0 +1,13 @@
+package com.csds341.project;
+
+public class Attachment {
+    public String name;
+    public String metadata;
+    public String data;
+
+    public Attachment(String name, String metadata, String data) {
+        this.name = name;
+        this.metadata = metadata;
+        this.data = data;
+    }
+}
