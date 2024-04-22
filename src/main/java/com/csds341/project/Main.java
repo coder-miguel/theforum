93f893886ad3c89012ff5a85ea7809198d8bdf42
kelifire <mom005@mymail.tc3.edu>
2024-04-21 23:48:30 -0400

    Cleanup

--- src/main/java/com/csds341/project/Main.java
+++ src/main/java/com/csds341/project/Main.java
@@ -115,7 +115,7 @@ public class Main {
             switch(selection) {
                 case 1:
                     user = existingUser();
-                    if (user != null) break;
+                    break;
                 case 2:
                     user = newUser();
                     break;
@@ -132,14 +132,23 @@ public class Main {
          */
         static String existingUser() {
             String loginAttempt = null;
+            int attemtps = 0;
             try {
                 if (theForum.getUsers().length == 0) {
                     System.out.println("No users found. Please create a user first.");
                 } else {
                     loginAttempt = MainMenu.selectUser("Select a user to log in as: ");
+                    if (loginAttempt == null) {
+                        return null;
+                    }
                     System.out.println("Enter in your password: ");
                     String password = scanner.nextLine();
                     while (!theForum.validatePassword(password, loginAttempt)) {
+                        attemtps++;
+                        if (attemtps >= 3) {
+                            System.out.println("Too many attempts. Please try again later.");
+                            return null;
+                        }
                         System.out.println("Invalid password. Please try again.");
                         password = scanner.nextLine();
                     }
@@ -162,7 +171,7 @@ public class Main {
             String username = scanner.nextLine();
             try {
                 while(theForum.userExists(username)) {
-                    System.out.println("Enter in your new username: ");
+                    System.out.println("Username already exists. Try again: ");
                     username = scanner.nextLine();
                 }
                 String password = "";
@@ -205,10 +214,11 @@ public class Main {
 
                 0) Exit
                 1) Create a new group
-                2) Join user to a group
-                3) Show group members
-                4) Post a thread
-                5) Post a reply
+                2) Join a group
+                3) Group members
+                4) Start a thread
+                5) Reply to a thread
+                6) Browse threads
 
                 """);
         }
@@ -221,12 +231,12 @@ public class Main {
          */
         static int select() {
             int selection = -1;
-            while (selection < 0 || selection > 5) {
+            while (selection < 0 || selection > 6) {
                 try {
                     selection = Integer.parseInt(scanner.nextLine());
                 } catch (NumberFormatException e) {
                     selection = -1;
-                    System.out.println("Invalid input. Please enter a number between 0 and 5.");
+                    System.out.println("Invalid input. Please enter a number between 0 and 6.");
                 }
             }
 
@@ -251,8 +261,10 @@ public class Main {
                             System.out.println("No groups found. Please create a group first.");
                         } else {
                             String group = MainMenu.selectGroup("Select a group: ", false);
-                            theForum.joinGroup(group, loggedinUser);
-                            theForum.getConn().commit();
+                            if (group != null) {
+                                theForum.joinGroup(group, loggedinUser);
+                                theForum.getConn().commit();
+                            }
                         }
                         break;
                         
@@ -274,39 +286,58 @@ public class Main {
                     /**
                      * 4: Post a thread
                      * 5: Post a reply
+                     * 6: View threads
                      */
                     case 4:
                     case 5:
+                    case 6:
                         // Inform user if no viewable threads exist
-                        if (selection == 5 && theForum.getThreads(loggedinUser).size() == 0) {
+                        if (selection > 4 && theForum.getThreads(loggedinUser).size() == 0) {
                             System.out.println("No threads found. Try creating a thread first.");
                         } else {
                             int selectedThread;
-                            // Make a new thread if option is 4
+
+                            // Make a new thread if seledction is 4
                             if (selection == 4) {
                                 System.out.println("Enter a title for the thread: ");
                                 String title = scanner.nextLine();
+                                if (title.length() == 0) title = "[No title]";
                                 selectedThread = theForum.createThread(Main.loggedinUser, title);
                                 if (theForum.getGroups(loggedinUser).length > 0) {
-                                    System.out.println("Add thread to a group? (y/n): ");
-                                    if (scanner.nextLine().equalsIgnoreCase("y")) {
-                                        String group = MainMenu.selectGroup("Select a group: ", true);
-                                        theForum.addThreadToGroup(selectedThread, group);
-                                    }
+                                    boolean addingGroup = false;
+                                    do {
+                                        System.out.println("Post thread to a group? y/(n): ");
+                                        String yn = scanner.nextLine();
+                                        if (yn.length() > 0 && String.valueOf(yn.charAt(0)).equalsIgnoreCase("y")) {
+                                            addingGroup = true;
+                                            String group = MainMenu.selectGroup("Select a group: ", true);
+                                            if (group != null) {
+                                                theForum.addThreadToGroup(selectedThread, group);
+                                                addingGroup = false;
+                                            }
+                                        } else {
+                                            addingGroup = false;
+                                        }
+                                    } while (addingGroup);
                                 }
-                            // Select a thread if option is 5
+                            // Select a thread if replying to an existing one
                             } else {
-                                selectedThread = selectThread("Select a thread to reply to: ");
+                                selectedThread = selectThread("View a thread: ");
                             }
 
+                            if (selectedThread == 0) return selection;
+                            
                             // Show all replies of the selected thread
                             for (String r : theForum.getReplies(selectedThread)) {
                                 System.out.println(r);
                             }
 
+                            if (selection == 6) return selection;
+
                             // Post a reply
                             System.out.println("Enter the content body: ");
                             String content = scanner.nextLine();
+                            if (content.length() == 0) content = "[Empty]";
                             int new_reply_id = theForum.postReply(selectedThread, Main.loggedinUser, content);
 
                             // Post attachments to the reply
@@ -324,9 +355,20 @@ public class Main {
                                 String metadata = scanner.nextLine();
                                 System.out.println("Enter the content of attachment " + (i + 1) + ": ");
                                 byte[] data = scanner.nextLine().getBytes();
+                                if (name.length() == 0)     name = "attachment" + i;
+                                if (metadata.length() == 0) metadata = "binary";
                                 theForum.createAttachment(new_reply_id, name, metadata, data);
                             }
-                            theForum.getConn().commit();
+
+                            String[] replies = theForum.getReplies(selectedThread);
+                            System.out.println(replies[replies.length - 1]);
+
+                            System.out.println("Post Reply? y/(n): ");
+                            String yn = scanner.nextLine();
+                            if (yn.length() > 0 && String.valueOf(yn.charAt(0)).equalsIgnoreCase("y"))
+                                theForum.getConn().commit();
+                            else
+                                theForum.getConn().rollback();
                         }
                         break;
                     default:
@@ -352,20 +394,26 @@ public class Main {
             } else {
                 System.out.println("Select a user:");
             }
+            System.out.println("0) Go Back");
             for (int i = 0; i < users.length; i++) {
                 System.out.println(String.valueOf(1 + i) + ") " + users[i]);
             }
-            int selectedUser = 0;
+            int selectedUser = -1;
             try {
                 selectedUser = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
+                selectedUser = -1;
             }
-            while (selectedUser <= 0 || selectedUser > users.length) {
-                System.out.println("Invalid input. Please enter a number between 1 and " + (users.length));
+            while (selectedUser < 0 || selectedUser > users.length) {
+                System.out.println("Invalid input. Please enter a number between 0 and " + (users.length));
                 try {
                     selectedUser = Integer.parseInt(scanner.nextLine());
                 } catch (NumberFormatException e) {
+                    selectedUser = -1;
                 }
+            }
+            if (selectedUser == 0) {
+                return null;
             }
             for (char c : users[selectedUser - 1].toCharArray()) {
                     sb.append(c);
@@ -386,21 +434,26 @@ public class Main {
             } else {
                 System.out.println("Select a group:");
             }
+            System.out.println("0) Go Back");
             for (int i = 0; i < groups.length; i++) {
                 System.out.println(String.valueOf(1 + i) + ") " + groups[i]);
             }
-            int selectedGroup = 0;
+            int selectedGroup = -1;
             try {
                 selectedGroup = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
-                selectedGroup = 0;
+                selectedGroup = -1;
             }
-            while (selectedGroup <= 0 || selectedGroup > groups.length) {
-                System.out.println("Invalid input. Please enter a number between 1 and " + (groups.length));
+            while (selectedGroup < 0 || selectedGroup > groups.length) {
+                System.out.println("Invalid input. Please enter a number between 0 and " + (groups.length));
                 try {
                     selectedGroup = Integer.parseInt(scanner.nextLine());
                 } catch (NumberFormatException e) {
+                    selectedGroup = -1;
                 }
+            }
+            if (selectedGroup == 0) {
+                return null;
             }
             for (char c : groups[selectedGroup - 1].toCharArray()) {
                     sb.append(c);
@@ -415,6 +468,7 @@ public class Main {
             } else {
                 System.out.println("Select a thread:");
             }
+            System.out.println("0) Go Back");
             for (HashMap.Entry<Integer, String> t : threads.entrySet()) {
                 System.out.println(String.valueOf(t.getKey()) + ") " + t.getValue());
             }
@@ -422,8 +476,9 @@ public class Main {
             try {
                 selectedThread = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
+                selectedThread = -1;
             }
-            while (!threads.keySet().contains(selectedThread)) {
+            while (!threads.keySet().contains(selectedThread) && selectedThread != 0) {
                 System.out.println("Invalid input. Please enter a number in the list above.");
                 try {
                     selectedThread = Integer.parseInt(scanner.nextLine());
